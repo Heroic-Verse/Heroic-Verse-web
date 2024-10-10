afdfd0b739194063244e3da62c42d0ae7f8941d4
Noah Beck <beck.863@buckeyemail.osu.edu>
2024-10-10 18:29:44 -0400

    Update Navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,21 +11,21 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
   return (
+    <Router>
     <div className="grid-container">
-<head>
     <title>Heroic Verse</title>
-</head>
-<link rel="stylesheet" href="style.css"></link>
-<body>
-    <div>
+    <link rel="stylesheet" href="style.css"></link>
+
+   <body>
+     <div>
         <header className="header">
             <div className="brand">
               <button onClick={openMenu}>&#9776;</button>
                 <a href="index.html">Heroic Verse</a>
             </div>
             <div className = "header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
+                <Link to="/">Home</Link>
+                <Link to="/catalog">Catalog</Link>
             </div>
         </header>
         <aside className="sidebar">
@@ -44,7 +44,13 @@ function App() {
            </ul> 
          
     </aside>
-        <main>
+
+    <main className="main">
+         <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/catalog" element={<Products />} />
+     </Routes>
+  </main>
             <div className="content">
                 <ul className="products">
                     <li>
@@ -93,15 +99,20 @@ function App() {
                     </li>
                 </ul>
             </div>
-        </main>
+        
         <footer className="footer">
             &copy; 2024 Heroic Verse
-        </footer>
-    </div>
-</body>
-    
-</div>
+            </footer>
+         </div>
+       </body>
+     </div>
+  </Router>
   );
 }
 
 export default App;
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+
