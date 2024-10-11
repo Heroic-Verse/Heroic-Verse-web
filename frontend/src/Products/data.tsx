f01dd7e075265e814dca0fc6d12ebb5a2acfe649
Soham Sorathia <Sorathia.6@buckeyemail.osu.edu>
2024-10-11 13:51:18 -0400

    up to the end of pg 26

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,28 @@
+import Products from "./Products";
+
+export default{
+
+    Products: [{
+
+        name:'Shirt (Small)',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },{
+        name:'Shirt (Medium)',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },{
+        name:'Shirt (Large)',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    }]
+}
